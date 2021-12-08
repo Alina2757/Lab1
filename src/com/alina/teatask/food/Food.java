fb7c2a30897347dd5be13b289f5c01bbe5d39e8a
Alina Okulich <alinaokulich000@mail.ru>
2021-12-08 16:10:39 +0300

    ADDED Food class

---
+++ src/com/alina/teatask/food/Food.java
@@ -0,0 +1,14 @@
+package com.alina.teatask.food;
+
+public class Food implements Nutritious {
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public int calculateCalories() {
+        return 0;
+    }
+}
